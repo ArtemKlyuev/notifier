75500a31d0b18c158850dd6efc6e19fc8a106d89
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-09-06 11:16:53 +0300

    refactor: `Notifier` types are separated into their own file

---
+++ packages/core/src/Notifier/types.ts
@@ -0,0 +1,40 @@
+import { Timer } from '../Timer';
+
+interface BaseNotification<Payload> {
+  id: string | number;
+  payload: Payload;
+}
+
+export interface PreparedNotification<Payload> extends BaseNotification<Payload> {
+  options?: Partial<BaseOptions>;
+}
+
+export interface LaunchedNotification<Payload> extends BaseNotification<Payload> {
+  options: BaseOptions;
+  info: {
+    timer: Timer | null;
+  };
+}
+
+export interface BaseOptions {
+  autoRemove: boolean;
+  autoRemoveTimeout: number;
+  persist: boolean;
+}
+
+export interface Options extends BaseOptions {
+  size: number;
+}
+
+export type NotificationEvent = 'add' | 'remove';
+
+export type Disposer = () => void;
+export type Handler = () => void;
+
+export interface Notifier<Payload> {
+  readonly notifications: LaunchedNotification<Payload>[];
+  setOptions: (options: Options) => void;
+  add: (notification: PreparedNotification<Payload>) => void;
+  remove: (id: string | number) => void;
+  subscribe: (event: NotificationEvent, handler: Handler) => Disposer;
+}
