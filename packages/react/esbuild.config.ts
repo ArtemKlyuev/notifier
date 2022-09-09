48196fbe3b550318406e42a8db01f90cb0269773
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-09-09 20:55:49 +0300

    build: add `esbuild` config

---
+++ packages/react/esbuild.config.ts
@@ -0,0 +1,39 @@
+import fs from 'fs/promises';
+
+import { build, BuildOptions } from 'esbuild';
+import { dTSPathAliasPlugin } from 'esbuild-plugin-d-ts-path-alias';
+
+import { devDependencies, peerDependencies } from './package.json';
+
+const DIST_DIR = 'dist';
+const EXTERNAL_PACKAGES = Object.keys({ ...devDependencies, ...peerDependencies });
+
+const baseOptions: BuildOptions = {
+  target: 'es2019',
+  external: EXTERNAL_PACKAGES,
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  treeShaking: true,
+  sourcemap: false,
+  minify: false,
+};
+
+const start = async (): Promise<void> => {
+  await fs.rm(DIST_DIR, { force: true, recursive: true });
+
+  build({
+    ...baseOptions,
+    splitting: true,
+    format: 'esm',
+    outdir: `${DIST_DIR}/esm`,
+    plugins: [dTSPathAliasPlugin({ outputPath: `${DIST_DIR}/typings`, debug: true })],
+  });
+
+  build({
+    ...baseOptions,
+    format: 'cjs',
+    outdir: `${DIST_DIR}/cjs`,
+  });
+};
+
+start();
