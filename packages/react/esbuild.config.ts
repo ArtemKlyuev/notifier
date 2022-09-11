88262b75cfe97ff1d94348897a09e9fc253d5ec7
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-09-11 17:40:07 +0300

    deps: add `@types/node`

--- packages/react/esbuild.config.ts
+++ packages/react/esbuild.config.ts
@@ -1,4 +1,4 @@
-import fs from 'fs/promises';
+import fs from 'node:fs/promises';
 
 import { build, BuildOptions } from 'esbuild';
 import { dTSPathAliasPlugin } from 'esbuild-plugin-d-ts-path-alias';
